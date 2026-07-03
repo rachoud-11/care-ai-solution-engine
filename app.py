973a8c91fc3b498921da0c1f89a40f96bc7932e8
Ramesh Choudhary <Ramesh.Choudhary@IDeaS.com>
2026-07-04 02:30:06 +0530

    design ready

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+sample_problem = {
+    "problem_id":1802408,
+    "client":"Hilton",
+    "property":"DoubleTree Jacksonville Airport",
+    "property_code":"JAXAR",
+    "job_name":"NGI CDP Deferred Delivery Job",
+    "job_instance":"446415369",
+    "owner":"Ramesh Choudhary",
+    "error_code":"UNEXPECTED_ERROR",
+    "description":"Error processing CP Recommendation : Connection pool shut down"
+}
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/problem/<problem_id>")
-def failed_job(problem_id):
-
-    return f"Problem {problem_id}"
+    return render_template(
+        "problem.html",
+        problem=sample_problem
+    )
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
-
-    
